e07e5b74e525266cf8fca988d551baa614f36915
zakiiizakaria <zakihelmi1997@gmail.com>
2025-04-07 16:05:48 +0800

    remove unuse files

--- public/simple-test.php
+++
@@ -1,4 +0,0 @@
-<?php
-// This is a very simple test file
-echo "PHP is working!";
-?>
--- public/test-connection.php
+++
@@ -1,5 +0,0 @@
-<?php
-// Simple test file to check if PHP is working
-header('Content-Type: application/json');
-echo json_encode(['status' => 'success', 'message' => 'PHP is working!']);
-?>
